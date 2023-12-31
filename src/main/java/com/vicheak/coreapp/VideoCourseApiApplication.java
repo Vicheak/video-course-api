4b12af25d12044b5dc3cb6a835bbdf8411a8db6d
Vicheak <suonvicheak991@gmail.com>
2023-12-31 16:19:10 +0700

    init project

---
+++ src/main/java/com/vicheak/coreapp/VideoCourseApiApplication.java
@@ -0,0 +1,13 @@
+package com.vicheak.coreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCourseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoCourseApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vicheak/coreapp/VideoCourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicheak.coreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCourseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
